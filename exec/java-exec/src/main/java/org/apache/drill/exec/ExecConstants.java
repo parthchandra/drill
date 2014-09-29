665850532077d37be79d96fc36673ada3f9ef325
Parth Chandra <pchandra@maprtech.com>
2014-09-29 16:11:14 -0700

    DRILL-1411: Disable feature by default. Add an overcommit factor to the fragment limit. Can be enabled and overcommit factor can be set as bootstrap parameters.

--- exec/java-exec/src/main/java/org/apache/drill/exec/ExecConstants.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/ExecConstants.java
@@ -74,6 +74,10 @@ public interface ExecConstants {
   public static final String SYS_STORE_PROVIDER_LOCAL_PATH = "drill.exec.sys.store.provider.local.path";
   public static final String SYS_STORE_PROVIDER_LOCAL_ENABLE_WRITE = "drill.exec.sys.store.provider.local.write";
   public static final String ERROR_ON_MEMORY_LEAK = "drill.exec.debug.error_on_leak";
+  /** Fragment memory planning */
+  public static final String ENABLE_FRAGMENT_MEMORY_LIMIT = "drill.exec.memory.enable_frag_limit";
+  public static final String FRAGMENT_MEM_OVERCOMMIT_FACTOR = "drill.exec.memory.frag_mem_overcommit_factor";
+
 
   public static final String CLIENT_SUPPORT_COMPLEX_TYPES = "drill.client.supports-complex-types";
 
@@ -172,4 +176,5 @@ public interface ExecConstants {
   public static final OptionValidator ENABLE_VERBOSE_ERRORS = new BooleanValidator(ENABLE_VERBOSE_ERRORS_KEY, false);
 
   public static final String BOOTSTRAP_STORAGE_PLUGINS_FILE = "bootstrap-storage-plugins.json";
+
 }
