d9ec6d3afd0868f891467f3f87bf9047e43ca3bb
Hanifi Gunes <hgunes@maprtech.com>
2014-12-08 13:41:17 -0800

    improved drill-on-spark samples

--- contrib/drill-on-spark/src/main/scala/org/apache/drill/rdd/Main.scala
+++ contrib/drill-on-spark/src/main/scala/org/apache/drill/rdd/Main.scala
@@ -1,6 +1,7 @@
 package org.apache.drill.rdd
 
 import org.apache.drill.rdd.DrillConversions._
+import org.apache.drill.rdd.complex.{CNull, CObject}
 import org.apache.spark.{SparkConf, SparkContext}
 
 
@@ -9,37 +10,36 @@ object Main extends App {
   val sc = new SparkContext(conf)
 
 
-  def flatJSON(): Unit = {
-    val sql = "SELECT * FROM cp.`employee.json`"
+  def queryJSON(): Unit = {
+    val sql = "SELECT * FROM dfs.`local`.`lil500.json`"
     val employees = sc.drillRDD(sql)
-    val fullNames = employees.map(r=>{
-      val first = r.first_name.value
-      val last = r.last_name.value
-      s"$first $last"
-    })
-    fullNames.foreach(println)
-    val count = fullNames.count()
-    println(s"total records found: $count")
 
+    employees.foreach(println)
+    val count = employees.count()
+    println(s"total records found: $count")
   }
 
-  def complexJSON(): Unit = {
-    val sql = "SELECT * FROM dfs.`/Users/hgunes/workspaces/mapr/incubator-drill/data/mobile-small.json`"
-    val rdd0 = sc.drillRDD(sql).cache()
-    val rdd1 = rdd0.zipWithIndex.map {
-      case (r, i) =>
-//        val ui = r.user_info
-//        s"$ui"
-        val (tid, date, cid) = (r.trans_id, r.date, r.user_info.cust_id) // r.date, r.user_info.cust_id)
-        s"$i $tid $date $cid"
-    }.cache
-    val count = rdd1.count
-    println(rdd1.first)
+  def queryRDD(): Unit = {
+    // iterate from 10 to 30 in 10 spark partitions
+    val mobile = sc.parallelize(10 until 30, 10)
+    val few = mobile.zipWithIndex.map { case (r, i) =>
+      CObject(("index", i), ("number", r))
+    }
+
+    sc.registerAsTable("simple", few)
+    val sql2 = "SELECT * FROM spark.simple t where t.index > 2 and t.index < 6 and number > 13"
+    val result = sc.drillRDD(sql2)
+
+    result.foreach { r =>
+      val (index, number, value) = (r.index, r.number, r.value)
+      println(s"$index -> $number :: $value")
+    }
+    val count = result.count
     println(s"total records found: $count")
     sc.stop()
   }
 
-  complexJSON
+  queryRDD
 }
 
 
