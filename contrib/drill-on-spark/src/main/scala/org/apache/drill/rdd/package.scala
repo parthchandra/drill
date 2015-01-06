8af11fbb7a1e9dcacfe7ec4e5af0f2f3adeeb71a
Ritesh Sinha <rsinha@Administrators-MacBook-Pro-6.local>
2015-01-05 17:57:32 -0800

    Revert "Revert " local commit...""
    This reverts commit f971c271bd926bb54593375078f0be920af7f548.

--- contrib/drill-on-spark/src/main/scala/org/apache/drill/rdd/package.scala
+++ contrib/drill-on-spark/src/main/scala/org/apache/drill/rdd/package.scala
@@ -1,10 +1,11 @@
 package org.apache.drill
 
-import org.apache.drill.rdd.complex.FieldReaderInfo
+import org.apache.drill.rdd.complex.FieldReaderBackend
+
 
 package object rdd {
   type DrillIncomingRowType = org.apache.drill.rdd.complex.ReadableRecord
   type DrillOutgoingRowType = org.apache.drill.rdd.complex.CObject
   type QueryManagerFactoryType[T] = () => org.apache.drill.rdd.complex.query.QueryManager[T]
-  type RecordFactoryType[T] = (FieldReaderInfo) => T
+  type RecordFactoryType[T] = (FieldReaderBackend) => T
 }
