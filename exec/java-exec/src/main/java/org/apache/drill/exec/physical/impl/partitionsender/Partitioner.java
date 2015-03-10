yufeldman/incubator-drill

DRILL-2210 Introducing multithreading capability to PartitonerSender

exec/java-exec/src/main/java/org/apache/drill/exec/physical/impl/partitionsender/Partitioner.java
@@ -37,13 +37,16 @@ public interface Partitioner {
                           OperatorStats stats,
                           SendingAccountor sendingAccountor,
                           OperatorContext oContext,
-                          StatusHandler statusHandler) throws SchemaChangeException;
+                          StatusHandler statusHandler,
+                          int offset, int count) throws SchemaChangeException;
 
   public abstract void partitionBatch(RecordBatch incoming) throws IOException;
   public abstract void flushOutgoingBatches(boolean isLastBatch, boolean schemaChanged) throws IOException;
   public abstract void initialize();
   public abstract void clear();
   public abstract List<? extends PartitionOutgoingBatch> getOutgoingBatches();
+  public abstract PartitionOutgoingBatch getOutgoingBatch(int index);
+  public abstract OperatorStats getStats();
 
   public static TemplateClassDefinition<Partitioner> TEMPLATE_DEFINITION = new TemplateClassDefinition<>(Partitioner.class, PartitionerTemplate.class);
 }

exec/java-exec/src/main/java/org/apache/drill/exec/server/options/SystemOptionManager.java
@@ -56,6 +56,7 @@ public class SystemOptionManager implements OptionManager {
       PlannerSettings.PRODUCER_CONSUMER_QUEUE_SIZE,
       PlannerSettings.HASH_SINGLE_KEY,
       PlannerSettings.IDENTIFIER_MAX_LENGTH,
+      PlannerSettings.PARTITION_SENDER_THREADS,
       ExecConstants.CAST_TO_NULLABLE_NUMERIC_OPTION,
       ExecConstants.OUTPUT_FORMAT_VALIDATOR,
       ExecConstants.PARQUET_BLOCK_SIZE_VALIDATOR,

parthchandra/drill

exec/java-exec/src/main/java/org/apache/drill/exec/physical/impl/partitionsender/Partitioner.java
@@ -37,13 +37,16 @@ public interface Partitioner {
                           OperatorStats stats,
                           SendingAccountor sendingAccountor,
                           OperatorContext oContext,
-                          StatusHandler statusHandler) throws SchemaChangeException;
+                          StatusHandler statusHandler,
+                          int start, int count) throws SchemaChangeException;
 
   public abstract void partitionBatch(RecordBatch incoming) throws IOException;
   public abstract void flushOutgoingBatches(boolean isLastBatch, boolean schemaChanged) throws IOException;
   public abstract void initialize();
   public abstract void clear();
   public abstract List<? extends PartitionOutgoingBatch> getOutgoingBatches();
+  public abstract PartitionOutgoingBatch getOutgoingBatch(int index);
+  public abstract OperatorStats getStats();
 
   public static TemplateClassDefinition<Partitioner> TEMPLATE_DEFINITION = new TemplateClassDefinition<>(Partitioner.class, PartitionerTemplate.class);
 }

exec/java-exec/src/main/java/org/apache/drill/exec/server/options/SystemOptionManager.java
@@ -59,6 +59,8 @@ public class SystemOptionManager implements OptionManager {
       PlannerSettings.IDENTIFIER_MAX_LENGTH,
       PlannerSettings.HASH_JOIN_SWAP,
       PlannerSettings.HASH_JOIN_SWAP_MARGIN_FACTOR,
+      PlannerSettings.PARTITION_SENDER_THREADS_FACTOR,
+      PlannerSettings.PARTITION_SENDER_MAX_THREADS,
       ExecConstants.CAST_TO_NULLABLE_NUMERIC_OPTION,
       ExecConstants.OUTPUT_FORMAT_VALIDATOR,
       ExecConstants.PARQUET_BLOCK_SIZE_VALIDATOR,