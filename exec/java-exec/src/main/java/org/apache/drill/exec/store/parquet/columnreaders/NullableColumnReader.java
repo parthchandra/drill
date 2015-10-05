03e7b669a996bc3064290f4df8c633992ec55ea5
Parth Chandra <parthc@apache.org>
2015-10-05 10:25:56 -0700

    DRILL-3871: Off by one error while reading binary fields with one terminal null in parquet.

--- exec/java-exec/src/main/java/org/apache/drill/exec/store/parquet/columnreaders/NullableColumnReader.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/store/parquet/columnreaders/NullableColumnReader.java
@@ -51,6 +51,124 @@ abstract class NullableColumnReader<V extends ValueVector> extends ColumnReader<
 
   @Override
   public void processPages(long recordsToReadInThisPass) throws IOException {
+    //int indexInOutputVector = 0;
+    readStartInBytes = 0;
+    readLength = 0;
+    readLengthInBits = 0;
+    recordsReadInThisIteration = 0;
+    vectorData = castedBaseVector.getBuffer();
+
+      // values need to be spaced out where nulls appear in the column
+      // leaving blank space for nulls allows for random access to values
+      // to optimize copying data out of the buffered disk stream, runs of defined values
+      // are located and copied together, rather than copying individual values
+
+      long runStart = pageReader.readPosInBytes;
+      int  runLength = -1;     // number of non-null records in this pass.
+      int  nullRunLength = -1; // number of consecutive null records that we read.
+      int currentDefinitionLevel;
+      //boolean lastValueWasNull;
+      //boolean lastRunBrokenByNull = false;
+      int readCount = 0; // the record number we last read. 
+      int writeCount = 0; // the record number we last wrote to the value vector.
+
+
+
+      while (readCount < recordsToReadInThisPass && writeCount < valueVec.getValueCapacity()){
+        // read a page if needed
+        if (!pageReader.hasPage()
+             || ((readStartInBytes + readLength >= pageReader.byteLength && bitsUsed == 0) &&
+                 definitionLevelsRead >= pageReader.currentPageCount)) {
+            if (!pageReader.next()) {
+                break;
+            }
+            definitionLevelsRead = 0;
+        }
+        /*
+        lastValueWasNull = true;
+        runLength = 0;
+        if (lastRunBrokenByNull ) {
+          //nullsFound = 1;
+          lastRunBrokenByNull = false;
+        //} else  {
+          //nullsFound = 0;
+        }
+        */
+        
+        //
+        // Let's skip the next run of nulls if any ...
+        //
+        nullRunLength=0;
+        currentDefinitionLevel = pageReader.definitionLevels.readInteger();
+        while ( currentDefinitionLevel < columnDescriptor.getMaxDefinitionLevel() 
+                  && readCount < recordsToReadInThisPass
+                  && writeCount+nullRunLength < valueVec.getValueCapacity()
+                  && definitionLevelsRead <= pageReader.currentPageCount
+              ) {
+          readCount++;
+          definitionLevelsRead++;
+          totalDefinitionLevelsRead++;
+          nullsFound++;
+          nullRunLength++;
+          currentDefinitionLevel = pageReader.definitionLevels.readInteger();
+          //TODO: This could be the last record in the page. 
+        }
+        //
+        // Write the nulls if any
+        //
+        int writerIndex = ((BaseDataValueVector) valueVec).getBuffer().writerIndex();
+        if ( dataTypeLengthInBits > 8  || (dataTypeLengthInBits < 8 && totalValuesRead + runLength % 8 == 0)){
+          castedBaseVector.getBuffer().setIndex(0, writerIndex + (int) Math.ceil( nullsFound * dataTypeLengthInBits / 8.0));
+        }
+        else if (dataTypeLengthInBits < 8){
+          rightBitShift += dataTypeLengthInBits * nullsFound;
+        }
+        writeCount+=nullsFound; // TODO: nullsFound seems to be the same as nullRunLength - consolidate
+        valuesReadInCurrentPass += nullsFound;
+
+        //
+        // Handle the run of non-null values
+        //
+        while ( currentDefinitionLevel >= columnDescriptor.getMaxDefinitionLevel() 
+                  && readCount < recordsToReadInThisPass
+                  && writeCount+runLength < valueVec.getValueCapacity()
+                  && definitionLevelsRead <= pageReader.currentPageCount
+              ) {
+          readCount++;
+          definitionLevelsRead++;
+          totalDefinitionLevelsRead++;
+          runLength++;
+          castedVectorMutator.setIndexDefined(writeCount); //set the nullable bit to indicate a non-null value
+          currentDefinitionLevel = pageReader.definitionLevels.readInteger();
+        }
+
+        //
+        // Write the non-null values
+        //
+        this.recordsReadInThisIteration = runLength;
+
+        // set up metadata
+        this.readStartInBytes = pageReader.readPosInBytes;
+        this.readLengthInBits = recordsReadInThisIteration * dataTypeLengthInBits;
+        this.readLength = (int) Math.ceil(readLengthInBits / 8.0);
+        readField( runLength);
+
+        // Are we incrementing this at the right time??? Should we do this after reading and writing the nulls?
+        recordsReadInThisIteration += nullsFound;
+
+        valuesReadInCurrentPass += runLength;
+        totalValuesRead += recordsReadInThisIteration;
+        pageReader.valuesRead += recordsReadInThisIteration;
+
+        pageReader.readPosInBytes = readStartInBytes + readLength;
+        writeCount+=runLength;
+
+      }
+    valuesReadInCurrentPass = writeCount;
+    valueVec.getMutator().setValueCount(valuesReadInCurrentPass);
+  }
+  /*
+  public void processPages(long recordsToReadInThisPass) throws IOException {
     int indexInOutputVector = 0;
     readStartInBytes = 0;
     readLength = 0;
@@ -68,9 +186,10 @@ abstract class NullableColumnReader<V extends ValueVector> extends ColumnReader<
       int currentDefinitionLevel;
       boolean lastValueWasNull;
       boolean lastRunBrokenByNull = false;
+
       while (indexInOutputVector < recordsToReadInThisPass && indexInOutputVector < valueVec.getValueCapacity()){
         // read a page if needed
-      if (!pageReader.hasPage()
+        if (!pageReader.hasPage()
             || ((readStartInBytes + readLength >= pageReader.byteLength && bitsUsed == 0) &&
           definitionLevelsRead >= pageReader.currentPageCount)) {
           if (!pageReader.next()) {
@@ -81,10 +200,10 @@ abstract class NullableColumnReader<V extends ValueVector> extends ColumnReader<
         lastValueWasNull = true;
         runLength = 0;
         if (lastRunBrokenByNull ) {
-          nullsFound = 1;
+          //nullsFound = 1;
           lastRunBrokenByNull = false;
-        } else  {
-          nullsFound = 0;
+        //} else  {
+          //nullsFound = 0;
         }
         // loop to find the longest run of defined values available, can be preceded by several nulls
         while(indexInOutputVector < recordsToReadInThisPass
@@ -112,6 +231,11 @@ abstract class NullableColumnReader<V extends ValueVector> extends ColumnReader<
             castedVectorMutator.setIndexDefined(indexInOutputVector - 1);
           }
         }
+        // take care of an edge case - if a page has exactly one null at the end of the page, the code breaks out
+        // of the above loop but does not count the last value.
+        //if(indexInOutputVector == recordsToReadInThisPass && (runLength+nullsFound) != recordsToReadInThisPass ){
+        //  nullsFound+=recordsToReadInThisPass-runLength;
+        //}
         valuesReadInCurrentPass += nullsFound;
 
         int writerIndex = ((BaseDataValueVector) valueVec).getBuffer().writerIndex();
@@ -139,6 +263,7 @@ abstract class NullableColumnReader<V extends ValueVector> extends ColumnReader<
     valueVec.getMutator().setValueCount(
         valuesReadInCurrentPass);
   }
+  */
 
   @Override
   protected abstract void readField(long recordsToRead);
