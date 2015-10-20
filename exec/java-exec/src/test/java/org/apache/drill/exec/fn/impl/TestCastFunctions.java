0d80b2280d658f6a71643fa4e3c9847ea146793c
Hsuan-Yi Chu <hsuanyi@usc.edu>
2015-10-20 10:01:08 -0700

    DRILL-3950: Add test case and bump calcite version to 1.4.0-drill-r5-1
    Fix is in CALCITE-922

--- exec/java-exec/src/test/java/org/apache/drill/exec/fn/impl/TestCastFunctions.java
+++ exec/java-exec/src/test/java/org/apache/drill/exec/fn/impl/TestCastFunctions.java
@@ -20,9 +20,8 @@ package org.apache.drill.exec.fn.impl;
 
 import org.apache.drill.BaseTestQuery;
 import org.apache.drill.common.util.FileUtils;
+import org.joda.time.Period;
 import org.junit.Test;
-
-import java.util.Date;
 
 public class TestCastFunctions extends BaseTestQuery {
 
@@ -52,4 +51,21 @@ public class TestCastFunctions extends BaseTestQuery {
       .build().run();
   }
 
+  @Test // DRILL-3950
+  public void testCastTimesInterval() throws Exception {
+    final String query = "select cast(r_regionkey as Integer) * (INTERVAL '1' DAY) as col \n" +
+        "from cp.`tpch/region.parquet`";
+
+    testBuilder()
+        .sqlQuery(query)
+        .ordered()
+        .baselineColumns("col")
+        .baselineValues(Period.days(0))
+        .baselineValues(Period.days(1))
+        .baselineValues(Period.days(2))
+        .baselineValues(Period.days(3))
+        .baselineValues(Period.days(4))
+        .build()
+        .run();
+  }
 }
