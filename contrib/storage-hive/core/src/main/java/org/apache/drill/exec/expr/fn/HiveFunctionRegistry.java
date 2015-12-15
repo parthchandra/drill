dbc1f0ebcd57e2ba8d8a4309f35fb66538585f99
vkorukanti <venki.korukanti@gmail.com>
2015-12-14 23:43:58 -0800

    Revert "MD-418: Upgrade Hive storage plugin to work with Hive 1.2 version."
    This reverts commit 4e9a9d56703b0a55e7e141e2797be18c4aa9fd72.

--- contrib/storage-hive/core/src/main/java/org/apache/drill/exec/expr/fn/HiveFunctionRegistry.java
+++ contrib/storage-hive/core/src/main/java/org/apache/drill/exec/expr/fn/HiveFunctionRegistry.java
@@ -179,9 +179,7 @@ public class HiveFunctionRegistry implements PluggableFunctionRegistry{
         nonDeterministicUDFs.contains(udfClazz));
     } catch (IllegalAccessException | InstantiationException e) {
       logger.debug("Failed to instantiate class", e);
-    } catch (final Exception | Error e) {
-      logger.warn("Failed to match Hive function", e);
-    }
+    } catch (Exception e) { /*ignore this*/ }
 
     return null;
   }
@@ -201,9 +199,7 @@ public class HiveFunctionRegistry implements PluggableFunctionRegistry{
         returnOI,
         Types.optional(ObjectInspectorHelper.getDrillType(returnOI)),
         nonDeterministicUDFs.contains(udfClazz));
-    } catch (final Exception | Error e) {
-      logger.warn("Failed to match Hive function", e);
-    }
+    } catch (Exception e) { /*ignore this*/ }
 
     return null;
   }
