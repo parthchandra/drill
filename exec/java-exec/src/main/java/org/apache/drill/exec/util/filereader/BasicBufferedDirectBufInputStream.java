7f2f3596d94bd4f6eb0bb85903bba1ba3b7c1ad6
Parth Chandra <parthc@apache.org>
2016-06-20 11:23:13 -0700

    Cleanup packages

--- exec/java-exec/src/main/java/org/apache/drill/exec/util/filereader/BasicBufferedDirectBufInputStream.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/util/filereader/BasicBufferedDirectBufInputStream.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package parquet.FileReader;
+package org.apache.drill.exec.util.filereader;
 
 import com.google.common.base.Preconditions;
 import io.netty.buffer.DrillBuf;
